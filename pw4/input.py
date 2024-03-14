72a946f048a19d75b403ac8424b9cf66e22979f1
VuTuanMinh22bi13317 <hkpotatoman@gmail.com>
2024-03-14 20:49:54 +0700

    fixing prac 4

--- pw4/input.py
+++ pw4/input.py
@@ -1,16 +1,2 @@
-def get_student_input():
-    student_id = input("Enter the ID of the student: ")
-    name = input("Enter the name of the student: ")
-    dob = input("Enter the date of birth of the student (in YYYY-MM-DD format): ")
-    return student_id, name, dob
-
-def get_course_input():
-    course_id = input("Enter the ID of the course: ")
-    name = input("Enter the name of the course: ")
-    return course_id, name
-
-def get_mark_input():
-    student_id = input("Enter the ID of the student: ")
-    course_id = input("Enter the ID of the course: ")
-    mark = float(input("Enter the mark for the student in the course: "))
-    return student_id, course_id, mark
+def get_input(prompt):
+    return input(prompt)
