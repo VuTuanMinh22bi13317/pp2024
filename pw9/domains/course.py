725b5972ca2e21b36ae7bec815e2a8b63a01d67f
VuTuanMinh22bi13317 <hkpotatoman@gmail.com>
2024-03-14 21:24:32 +0700

    fixing prac 9

--- pw9/domains/course.py
+++ pw9/domains/course.py
@@ -1,10 +1,4 @@
-import curses
-
-
 class Course:
-    def __init__(self, course_id, name, credits):
+    def __init__(self, course_id, name):
         self.course_id = course_id
         self.name = name
-        self.credits = credits
-
-        
